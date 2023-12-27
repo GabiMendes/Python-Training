87a2fe757d59f9fc6512c4d4c1c9a658d1add092
Gabriela Mendes <gabi06marquesmd@gmail.com>
2023-12-27 19:12:39 -0300

    8kyu

--- 8kyu-InvertValues.py
+++ 8kyu-InvertValues.py
@@ -4,11 +4,13 @@
 # invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 # invert([]) == []
 
-# You can assume that all values are integers. Do not mutate the input array/list.
+# You can assume that all values are integers. 
+#>>Do not mutate the input array/list.<<
 
 def invert(lst):
+    lst_copy = lst.copy()
     position = 0
-    while position < len(lst):
-        lst[position] = -(lst[position])
+    while position < len(lst_copy):
+        lst_copy[position] = -(lst_copy[position])
         position += 1
-    return lst
+    return lst_copy
